829e224911ef2e633f156905236260f1360870fa
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-25 22:15:41 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get '/songs', to: 'songs#index'
+ get '/songs', to: 'songs#index'
   get '/songs/new', to: 'songs#new', as: 'new_song'
-  get '/songs/edit', to: 'songs#edit', as: 'edit_song'
-  get '/songs/'
+  post '/songs', to: 'songs#create'
+  get '/songs/:id', to: 'songs#show', as: 'song'
+  get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
+  patch '/songs/:id', to: 'songs#update'
+  delete '/songs/:id', to: 'songs#destroy'
 end
